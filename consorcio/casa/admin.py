3e8674465748eff277d4ae1324629204d0f3f62b
javitoks <javier.dellorto@gmail.com>
2022-06-26 12:21:43 -0300

    en desarrollo

--- consorcio/casa/admin.py
+++ consorcio/casa/admin.py
@@ -3,8 +3,22 @@ from casa.models import *
 
 # Register your models here.
 
+#Para que aparezcan los campos "created" y "updated" y decirle q son de SOLO LECTURA, tenemos que crear una clase:
 
-admin.site.register(Casa)
-admin.site.register(Inquilino)
+class CasaAdmin(admin.ModelAdmin):
+    readonly_fields = ('creado', 'modificado')
+
+class InquilinoAdmin(admin.ModelAdmin):
+    readonly_fields = ('creado', 'modificado')
+
+class PropietarioAdmin(admin.ModelAdmin):
+    readonly_fields = ('creado', 'modificado')
+
+class AdministradorAdmin(admin.ModelAdmin):
+    readonly_fields = ('creado', 'modificado')
+
+
+admin.site.register(Casa, CasaAdmin)
+admin.site.register(Inquilino, InquilinoAdmin)
 admin.site.register(Propietario)
-admin.site.register(Administrador)
+admin.site.register(Administrador, AdministradorAdmin)
--- consorcio/casa/models.py
+++ consorcio/casa/models.py
@@ -1,60 +1,78 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.  
 
 
 
 
 class Administrador(models.Model):
-    nombre = models.CharField(max_length=30, blank=False)
-    apellido = models.CharField(max_length=30, blank=False)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30, blank=True)
     telefono = models.CharField(max_length=10, blank=True)
     email = models.EmailField(blank=True)
-    #administradas = models.ManyToManyField(Casa)
+    creado = models.DateTimeField(auto_now_add=True) 
+    modificado = models.DateTimeField(auto_now=True)
 
    
     class Meta:
         verbose_name = ("Administrador")
         verbose_name_plural = ("Administradores")
     
-        def __str__(self):
-            return self.nombre
+    def __str__(self):
+        return ('{} {}'.format(self.apellido, self.nombre))
     
 
 class Propietario(models.Model):
-    nombre = models.CharField(max_length=30, blank=False)
-    apellido = models.CharField(max_length=30, blank=False)
+    nombre = models.CharField(max_length=30, verbose_name='Propietario')
+    apellido = models.CharField(max_length=30, blank=True)
     telefono = models.CharField(max_length=10, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     CBU = models.CharField(max_length=22, blank=True, null=True)
-    #propiedades = models.ManyToManyField(Casa)
+    creado = models.DateTimeField(auto_now_add=True) 
+    modificado = models.DateTimeField(auto_now=True)
 
     class Meta:
-        verbose_name = ("Propietario")
-        verbose_name_plural = ("Propietarios")
+        ordering = ['nombre']   
+        verbose_name = 'Propietarios'
+        verbose_name_plural = "Propietarios"
     
-        def __str__(self):
-            return self.nombre
+    def __str__(self):
+        return ('{} {}'.format(self.apellido, self.nombre))
+        
 
 class Inquilino(models.Model):
-    nombre = models.CharField(max_length=30, blank=False)
-    apellido = models.CharField(max_length=30, blank=False)
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30, blank=True)
     telefono = models.CharField(max_length=10, blank=True, null=True)
     email = models.EmailField(blank=True, null=True)
     CBU = models.CharField(max_length=22, blank=True, null=True)
+    creado = models.DateTimeField(auto_now_add=True) 
+    modificado = models.DateTimeField(auto_now=True)
     
     class Meta:
         verbose_name = ("Inquilino")
         verbose_name_plural = ("Inquilinos")
     
-        def __str__(self):
-            return self.nombre
+    def __str__(self):
+        return ('{} {}'.format(self.apellido, self.nombre))
 
 
 class Casa(models.Model):
     numero_casa = models.CharField(max_length=2, blank=False, null=False, help_text='Ingrese el numero y la letra ej: 5A')
-    propietario = models.ForeignKey(Propietario, verbose_name=("Propietario"), on_delete=models.CASCADE)
-    administrador = models.ForeignKey(Administrador, verbose_name=("Administrador"), on_delete=models.CASCADE)
+    propietario = models.ForeignKey(Propietario, on_delete=models.CASCADE)  
+    administrador = models.ForeignKey(Administrador, on_delete=models.CASCADE)
     opciones_estado = [('ALQ', 'Alquilada'), ('DES' , 'Desocupada') , ('OCU', 'Ocupada-Dueño')]
-    estado = models.BooleanField(null=False, choices=opciones_estado, default='OCU')
-    inquilino = models.ForeignKey(Inquilino, verbose_name=("Inquilino"), on_delete=models.CASCADE)     
+    estado = models.CharField(max_length=3,  null=False, choices=opciones_estado, default='OCU')
+    inquilino = models.ForeignKey(Inquilino, verbose_name=("Inquilino"), on_delete=models.CASCADE)
+    disponibilidad = models.BooleanField(default=True)
+    creado = models.DateTimeField(auto_now_add=True) 
+    modificado = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = ("Casa")
+        verbose_name_plural = ("Casas")
+    
+    def __str__(self):
+        return ('{} {}'.format('Duplex', self.numero_casa))
+        
+         
