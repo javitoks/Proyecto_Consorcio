89a274d83c2bf766fe35fc050b5bf9ca6d42fe45
javitoks <javier.dellorto@gmail.com>
2022-06-26 23:00:53 -0300

    domingo

--- consorcio/casa/urls.py
+++ consorcio/casa/urls.py
@@ -5,11 +5,23 @@ from casa import views
 
 
 urlpatterns = [
-    path('casas/', views.listado_casas, name='listado_casas'),
-    path('propietarios/', views.listado_propietarios, name='propietarios'),
-    path('inquilinos/', views.listado_inquilinos, name='inquilinos'),
+    #path('casas/', views.listado_casas, name='listado_casas'),
+    #path('propietarios/', views.listado_propietarios, name='propietarios'),
+    #path('inquilinos/', views.listado_inquilinos, name='inquilinos'),
 
     path('registrar_propietario/', views.FormularioPropietarioView.index, name='registrarpropietario'),
-    path('guardar_propietario/', views.FormularioPropietarioView.procesar_formulario, name='guardar_propietario')
+    path('guardar_propietario/', views.FormularioPropietarioView.procesar_formulario, name='guardar_propietario'),
+    path('propietarios/', views.FormularioPropietarioView.listado_propietarios, name='propietarios'),
+    path('casas/', views.FormularioPropietarioView.listado_casas, name='listado_casas'),
+    path('inquilinos/', views.FormularioPropietarioView.listado_inquilinos, name='inquilinos'),
+    path('editar_propietario/<int:id_propietario>', views.FormularioPropietarioView.editar_propietario, name='editarpropietario'),
+    path('actualizar_propietario/<int:id_propietario>', views.FormularioPropietarioView.actualizar_propietario, name='actualizarpropietario')
+
+
+
+
+
+
+
 ]
 
--- consorcio/casa/views.py
+++ consorcio/casa/views.py
@@ -5,42 +5,54 @@ from casa.models import Casa, Inquilino, Propietario
 
 # Create your views here.
 
+
 class FormularioPropietarioView(HttpRequest):
-    
+
     def index(request):
         propietario = FormularioPropietario()
-        return render(request, 'form_propietario.html', {'form':propietario})
-    
+        return render(request, 'registrarpropietario.html', {'form': propietario})
+
     def procesar_formulario(request):
         propietario = FormularioPropietario(request.POST)
         if propietario.is_valid():
             propietario.save()
             propietario = FormularioPropietario()
-        return render(request, 'form_propietario.html', {'form': propietario, 'mensaje':'OK'})
+        return render(request, 'registrarpropietario.html', {'form': propietario, 'mensaje': 'OK'})
 
+    def editar_propietario(request, id_propietario):
+        propietario = Propietario.objects.filter(id=id_propietario).first()
+        form = FormularioPropietario(instance = propietario)
+        return render(request, 'editarpropietario.html', {'form':form, 'propietario':propietario})
 
+    def actualizar_propietario(request, id_propietario):
+        propietario = Propietario.objects.get(pk=id_propietario)
+        form = FormularioPropietario(request.POST, instance=propietario)
+        if form.is_valid():
+            form.save()
+        propietarios = Propietario.objects.all()
+        return render(request, 'propietarios.html', {'propietarios': propietarios})
+    
+    
+    
+    #por ahora estan aca
+    
+    def listado_propietarios(request):
+        data = {
+            'titulo': 'Listado de Propietarios',
+            'propietarios': Propietario.objects.all()
+        }
+        return render(request, 'propietarios.html', data)
 
+    def listado_casas(request):
+        data = {
+            'titulo': 'Listado de Casas',
+            'casas': Casa.objects.all()
+        }
+        return render(request, 'listado_casas.html', data)
 
-
-
-def listado_casas(request):
-    data = {
-        'titulo': 'Listado de Casas',
-        'casas': Casa.objects.all()
-    }
-    return render(request, 'listado_casas.html', data)
-
-def listado_propietarios(request):
-    data = {
-        'titulo': 'Listado de Propietarios',
-        'propietarios': Propietario.objects.all()
-    }
-    return render(request, 'propietarios.html', data)
-
-def listado_inquilinos(request):
-    data = {
-        'titulo': 'Listado de Propietarios',
-        'inquilinos': Inquilino.objects.all()
-    }
-    return render(request, 'inquilinos.html', data)
-
+    def listado_inquilinos(request):
+        data = {
+            'titulo': 'Listado de Propietarios',
+            'inquilinos': Inquilino.objects.all()
+        }
+        return render(request, 'inquilinos.html', data)
